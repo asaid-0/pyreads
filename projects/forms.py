1a4d574f7ba154d812c0a5fa7e9d4e0f73d0685f
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-04-22 22:26:18 +0200

    -display date picker for date fields in add project form and edit profile form -validate proect end date isgreater than start date

--- projects/forms.py
+++ projects/forms.py
@@ -18,8 +18,17 @@ class AddProjectForm(forms.ModelForm):
         widgets = {
             "tags": forms.TextInput(attrs={"data-role": "tagsinput", "name": "tags"}),
             "details": forms.Textarea(attrs={"rows": 7, "style": "resize:none;"}),
+            "start_date": forms.DateInput(attrs={"type": "date"}),
+            "end_date": forms.DateInput(attrs={"type": "date"}),
         }
 
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+        if end_date <= start_date:
+            msg = "End date should be greater than start date."
+            self.add_error("end_date", msg)
 
 class ImageForm(forms.ModelForm):
     class Meta:
@@ -35,7 +44,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ("content",)
-        labels = {"content": ''}
+        labels = {"content": ""}
         widgets = {
-            "content": forms.TextInput(attrs={"class": "form-control", "style": "border: none; border-radius: 0", "placeholder": 'New Comment....'}),
+            "content": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "style": "border: none; border-radius: 0",
+                    "placeholder": "New Comment....",
+                }
+            ),
         }
