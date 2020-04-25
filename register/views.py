49a69fd8fb3029b845b609687e18dbab736e26fc
moazGalbat <moaz.galbat@gmail.com>
2020-04-25 23:00:56 +0200

    fix bug in activation link

--- register/views.py
+++ register/views.py
@@ -49,7 +49,7 @@ def register(request):
     return render(request, "registration/register.html", {"form":form})
 
 
-def activate(request, uidb64, token):
+def activate(request, uidb64, token,backend='django.contrib.auth.backends.ModelBackend'):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
@@ -59,7 +59,7 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.signup_confirmation = True
         user.save()
-        login(request, user)
+        login(request, user,backend='django.contrib.auth.backends.ModelBackend')
         messages.success(request, 'Thank you for your email confirmation.')
         return redirect('home')
     else:
