b21bb9c10721ce5d3b930f4a6539a533c2b2946c
MohamadFeeshar <mohamed.feeshar@gmail.com>
2020-04-26 07:35:29 +0200

    show current rating as stars, add/update rating with ajax

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_project, view_project, add_comment, delete_project, get_category_projects, add_donation, report_comment, report_project
+from .views import add_project, view_project, add_comment, delete_project, get_category_projects, add_donation, report_comment, report_project, add_rate
 
 urlpatterns = [
     path('project/new', add_project, name='add_project'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('project/ajax/donation', add_donation, name='donate'),
     path('comment/<int:id>/report', report_comment, name='report_comment'),
     path('project/<int:id>/report', report_project, name='report_project'),
+    path('project/ajax/rate', add_rate, name='rate')
 ]
